a87021c528ae149b03833942d96b86b3a053fdb0
daniilfatahi <daniil.fatahi@gmail.com>
2022-09-08 19:50:24 +0200

    NewText

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -3,12 +3,8 @@ package com.company;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello Daniil");
+
 
     }
 }
